5c5217012bda0c7571e69b5ec08e1aef74584e49
ShamsiddinWeb <shamsiddinnorov734@gmail.com>
2024-11-12 18:28:15 +0500

    admin

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,16 @@ import { ToastContainer } from "react-toastify";
 import { Routes, Route } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import Login from "./components/login/Login";
-
 import Layout from "./components/layout/Layout";
 import Dashboard from "./pages/Admin/dashboard/dashboard";
 import Brands from "./pages/Admin/brands/Brands";
-import Locations from "./pages/Admin/locations/Locations"; 
 import Cars from "./pages/Admin/cars/Cars";
 import Cities from "./pages/Admin/cities/Cities";
 import Models from "./pages/Admin/models/Models";
 import "./App.scss";
 import Settings from "./pages/Admin/Settings/Settings"; 
 import Outletpage from "./api/Outletpage";
+import Locations from "./pages/Admin/loactions/Locations"
 
 export default function App() {
   return (
@@ -28,7 +27,7 @@ export default function App() {
               <Route path="/settings" element={<Settings />} />
               <Route path="/brands" element={<Brands />} />
               <Route path="/models" element={<Models />} />
-              <Route path="/locations" element={<Locations />} />
+              <Route path="/locations" element={<Locations/>} />
               <Route path="/cities" element={<Cities />} />
               <Route path="/cars" element={<Cars />} />
             </Route>
